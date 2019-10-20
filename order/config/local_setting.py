e8f97e07dff7483fea69f5fa85c45d17d198b44a
gear42 <870690672@qq.com>
2019-10-20 22:21:50 +0800

    modified:   order/config/base_setting.py 	modified:   order/config/local_setting.py 	modified:   order/config/production_setting.py

--- order/config/local_setting.py
+++ order/config/local_setting.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 DEBUG = True
 SQLALCHEMY_ECHO = True
-SQLALCHEMY_DATABASE_URI = 'mysql://root:@127.0.0.1/food_db?charset=utf8mb4'
+SQLALCHEMY_DATABASE_URI = 'mysql://food_db:3077ea57b05f710c@127.0.0.1/food_db?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENCODING = "utf8mb4"
